f1c18fe06298e207dac93a0bafdb6db71b554383
Kabiru Abdulhamid <abdulhamidkabiru@gmail.com>
2025-08-04 05:15:59 +0100

    chore: configure GitHub Actions workflow with secrets for Terraform variables

--- 04-rds-postgres/main.tf
+++ 04-rds-postgres/main.tf
@@ -1,29 +1,16 @@
-provider "aws" {
-  region = var.region
+resource "aws_db_instance" "default" {
+  allocated_storage    = 20
+  engine               = "mysql"
+  instance_class       = "db.t3.micro"
+  name                 = "mydb"
+  username             = "admin"
+  password             = var.db_password
+  vpc_security_group_ids = [var.rds_security_group_id]
+  db_subnet_group_name = aws_db_subnet_group.default.name
+  skip_final_snapshot  = true
 }
 
-resource "aws_db_subnet_group" "rds_subnet_group" {
-  name       = "rds-subnet-group"
-  subnet_ids = local.private_subnet_ids_list
-
-  tags = var.tags
-}
-
-resource "aws_db_instance" "postgres" {
-  identifier              = var.db_identifier
-  allocated_storage       = var.allocated_storage
-  engine                  = "postgres"
-  engine_version          = var.engine_version
-  instance_class          = var.instance_class
-  name                    = var.db_name
-  username                = var.db_username
-  password                = var.db_password
-  db_subnet_group_name    = aws_db_subnet_group.rds_subnet_group.name
-  vpc_security_group_ids  = [var.rds_security_group_id]
-  skip_final_snapshot     = true
-  publicly_accessible     = false
-  multi_az                = false
-  backup_retention_period = 7
-
-  tags = var.tags
+resource "aws_db_subnet_group" "default" {
+  name       = "my-db-subnet-group"
+  subnet_ids = var.private_subnet_ids
 }
--- 04-rds-postgres/variables.tf
+++ 04-rds-postgres/variables.tf
@@ -1,71 +1,20 @@
-variable "region" {
-  description = "AWS region"
-  type        = string
-  default     = "eu-north-1"
-}
-
-variable "private_subnet_ids" {
-  description = "Comma-separated list of private subnet IDs"
-  type        = string
+variable "ec2_security_group_id" {
+  type = string
+  description = "Security group for EC2 instances"
 }
 
 variable "rds_security_group_id" {
-  description = "Security group ID to associate with RDS"
-  type        = string
+  type = string
+  description = "Security group for RDS instances"
 }
 
-variable "db_identifier" {
-  description = "The RDS instance identifier"
-  type        = string
-  default     = "my-postgres-db"
-}
-
-variable "allocated_storage" {
-  description = "Allocated storage in GB"
-  type        = number
-  default     = 20
-}
-
-variable "engine_version" {
-  description = "Postgres engine version"
-  type        = string
-  default     = "15.3"
-}
-
-variable "instance_class" {
-  description = "RDS instance class"
-  type        = string
-  default     = "db.t3.micro"
-}
-
-variable "db_name" {
-  description = "Database name"
-  type        = string
-  default     = "mydatabase"
-}
-
-variable "db_username" {
-  description = "Database username"
-  type        = string
-  default     = "admin"
+variable "private_subnet_ids" {
+  type = list(string)
+  description = "List of private subnet IDs"
 }
 
 variable "db_password" {
+  type      = string
+  sensitive = true
   description = "Database password"
-  type        = string
-  sensitive   = true
-}
-
-variable "tags" {
-  description = "Tags to apply to all resources"
-  type        = map(string)
-  default = {
-    Environment = "dev"
-    Owner       = "kabiru"
-    Project     = "terraform-series"
-  }
-}
-
-locals {
-  private_subnet_ids_list = split(",", var.private_subnet_ids)
 }
